e8bd5e5e81cafed1c5d30b4b2072baab88a71fb1
PalomaGuth <115418028+paloma-guth@users.noreply.github.com>
2026-04-04 16:29:57 +0000

    Fix: Remove base path for user site deployment

--- planet-earth/astro.config.mjs
+++ planet-earth/astro.config.mjs
@@ -3,10 +3,6 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  // 1. Keep your Integrations
   integrations: [react()],
-  
-  // 2. Add your Deployment Info
   site: 'https://paloma-guth.github.io',
-  // base: '/portfolio', 
 });
