7bbc66c4d45e2195258d1962f782b9bab2768c83
PalomaGuth <115418028+paloma-guth@users.noreply.github.com>
2026-04-04 18:31:24 +0000

    fix: update site URL for project repo

--- planet-earth/astro.config.mjs
+++ planet-earth/astro.config.mjs
@@ -4,5 +4,5 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  site: 'https://paloma-guth.github.io',
+  site: 'https://paloma-guth.github.io/portfolio',
 });
